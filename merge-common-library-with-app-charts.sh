2f28dc8018278ccfdedb68e9ddada6a4e9d7fc95
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-06-15 03:02:29 -0700

    add merge-common-library-with-app-charts.sh

---
+++ merge-common-library-with-app-charts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# TODO updage common library git submodule
+library_common_dir=library-charts/library/common
+
+
+for dir in charts/stable/sonarr charts/stable/radarr; do
+    app_common_dir=$dir/charts
+    rm -rf $app_common_dir
+    mkdir -p $app_common_dir
+    cp -r $library_common_dir $app_common_dir
+    git add $app_common_dir
+done
+
+git status
+
