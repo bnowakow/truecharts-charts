81de02e918f5862a1f2e30774a674deff5375f41
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-06-15 03:07:25 -0700

    wip

--- merge-common-library-with-app-charts.sh
+++ merge-common-library-with-app-charts.sh
@@ -4,7 +4,11 @@
 library_common_dir=library-charts/library/common
 
 
-for dir in charts/stable/sonarr charts/stable/radarr; do
+for dir in \
+        charts/stable/sonarr \
+        charts/stable/radarr \
+        ./charts/stable/bazarr \
+    ; do
     app_common_dir=$dir/charts
     rm -rf $app_common_dir
     mkdir -p $app_common_dir
