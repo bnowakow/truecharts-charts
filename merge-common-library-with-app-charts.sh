62c0b91145f8d9e3968e218808ff1b180033a4f5
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-06-15 04:40:58 -0700

    add apps

--- merge-common-library-with-app-charts.sh
+++ merge-common-library-with-app-charts.sh
@@ -10,6 +10,8 @@ for dir in \
         charts/stable/bazarr \
         charts/stable/overseerr \
         charts/stable/jellyfin \
+        charts/stable/jackett \
+        ./charts/stable/prowlarr \
     ; do
     app_common_dir=$dir/charts
     rm -rf $app_common_dir
