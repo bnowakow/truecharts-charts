8681b79a8495f26aca7e19280990dfc1c7a7a82f
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-08-09 00:30:36 -0700

    update

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 git checkout master \
     && git pull \
     && git checkout bnowakow \
-    && git merge master \
+    && git merge master --no-edit \
     && ./merge-common-library-with-app-charts.sh \
     && git push \
     && git status
