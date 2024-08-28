cd33d32d877027cdafa83453b10f184b1fd4c4e1
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-08-28 12:00:29 -0700

    update

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ cd /mnt/MargokPool/home/sup/code/bash_configs/repos/truecharts
 
 git checkout master \
     && git pull \
+    && git push \
     && git checkout bnowakow \
     && git merge master --no-edit \
     && ./merge-common-library-with-app-charts.sh \
