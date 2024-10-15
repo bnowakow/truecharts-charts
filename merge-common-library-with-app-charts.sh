ecdbfe4996b513e30073a37fe6639d3bc59b30eb
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-10-15 05:00:50 -0700

    update

--- merge-common-library-with-app-charts.sh
+++ merge-common-library-with-app-charts.sh
@@ -50,7 +50,8 @@ for dir in \
     cd $dir
     rm Chart.lock
     helm dependency build
-    helm package .
+    cd charts
+    helm package ../
     cd $main_dir
     git add $app_common_dir
     echo
